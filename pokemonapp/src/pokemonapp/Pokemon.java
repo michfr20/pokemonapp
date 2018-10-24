3cc8f9e62931f71e1855b9098c094d1cb26af2ae
Tomy <tomyfaro2000@gmail.com>
2018-10-24 11:40:14 -0500

    Se agrego una nueva clase

---
+++ pokemonapp/src/pokemonapp/Pokemon.java
@@ -0,0 +1,28 @@
+
+
+package pokemonapp;
+
+/**
+ *
+ * @author alumno
+ */
+public class Pokemon {
+    private String nombre;
+    private int nivel;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+}
