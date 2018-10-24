97658bd670b7139fb0a187cd5caf19958ac00a08
Tomy <tomyfaro2000@gmail.com>
2018-10-24 12:01:18 -0500

    Se agrego la clase pokebola

--- pokemonapp/src/pokemonapp/Pokemonapp.java
+++ pokemonapp/src/pokemonapp/Pokemonapp.java
@@ -11,6 +11,9 @@ public class Pokemonapp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola pokemon");
+        
+        Pokemon hueso = new Pokemon();
+        
     }
     
 }
