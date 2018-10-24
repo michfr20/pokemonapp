5318a8235558e2dd8515147bdf2d729947e13db1
alumno informatica <alumno@ciis.usil.edu.pe>
2018-10-24 11:26:25 -0500

    Versión inicial de pokemonapp

---
+++ pokemonapp/src/pokemonapp/Pokemonapp.java
@@ -0,0 +1,16 @@
+
+
+package pokemonapp;
+
+
+
+public class Pokemonapp {
+
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola pokemon");
+    }
+    
+}
